6f016e4258b69e3b39fd029a9768fd1717a8a3f5
Jarod Wilson <jaroddwilson@gmail.com>
2021-10-20 14:48:45 -0400

    commit tree d0cde2af10a97cb3f723c9f41cdee5d8ae20aae5

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+function findMatching(nameList, matchName){
+    return nameList.filter(name => name.toUpperCase() === matchName.toUpperCase());
+}
+
+function fuzzyMatch(nameList, firstLetters){
+    return nameList.filter(name => name.indexOf(firstLetters) === 0);
+}
+
+function matchName(driverArray, matchName){
+    return driverArray.filter(obj => obj.name === matchName);
+}
